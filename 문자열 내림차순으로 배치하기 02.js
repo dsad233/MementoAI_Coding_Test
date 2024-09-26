49a3101379a092da489b7d79a8adb82fc9453a93
dsad233 <ksl1421@naver.com>
2024-09-26 16:26:19 +0900

    2번 문제 풀이 완료

---
+++ 문자열 내림차순으로 배치하기 02.js
@@ -0,0 +1,19 @@
+// 문자열 내림차순으로 배치하기 02
+
+function solution(s) {
+    var answer = '';
+    let upString = '';
+    const split = s.split('').sort((a, b) => b.localeCompare(a));
+    
+    for(let i = 0; i < split.length; i++){
+        if(split[i].toUpperCase() !== split[i]){
+            answer += split[i];
+        } else {
+            upString += split[i];
+        }
+    }
+    
+    answer += upString;
+    
+    return answer;
+}
